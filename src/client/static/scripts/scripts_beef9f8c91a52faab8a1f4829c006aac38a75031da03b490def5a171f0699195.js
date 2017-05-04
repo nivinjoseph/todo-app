c8546b8cce56285a58599c3dee2d7c0da435a457
nivinjoseph <nivinjoseph@outlook.com>
2017-05-04 11:40:24 -0400

    fixed scss compile watch issue

--- src/client/static/scripts/scripts_beef9f8c91a52faab8a1f4829c006aac38a75031da03b490def5a171f0699195.js
+++ src/client/static/scripts/scripts_beef9f8c91a52faab8a1f4829c006aac38a75031da03b490def5a171f0699195.js
@@ -25734,30 +25734,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const n_app_1 = __webpack_require__(4);
 let TodoViewModel = class TodoViewModel extends n_app_1.ComponentViewModel {
-    constructor() {
-        super();
-        console.log(" TodoViewModel");
-    }
     get todo() { return this.getBound("value"); }
     editTodo() {
         this.getBound("editValue")(this.todo);
     }
     deleteTodo() {
-        console.log("deleting todo");
         this.getBound("deleteValue")(this.todo);
     }
 };
 TodoViewModel = __decorate([
     n_app_1.element("todo"),
     n_app_1.bind("value", "editValue", "deleteValue"),
-    n_app_1.view("todo-view"),
-    __metadata("design:paramtypes", [])
+    n_app_1.view("todo-view")
 ], TodoViewModel);
 exports.TodoViewModel = TodoViewModel;
 //# sourceMappingURL=todo-view-model.js.map
