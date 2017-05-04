bf62302a157be6ccbbb928ee27cd5e365d075a37
nivinjoseph <nivinjoseph@outlook.com>
2017-05-04 09:15:29 -0400

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,10 @@ import { DefaultController } from "./controllers/default-controller";
 import { ConfigurationManager } from "n-config";
 
 
-console.log("mode", ConfigurationManager.getConfig("mode"));
-
 const app = new WebApp(ConfigurationManager.getConfig<number>("port"))
     .useViewResolutionRoot("src/controllers")
     .registerStaticFilePaths("src/client/dist")
     .registerControllers(DefaultController);
-    
-
 
 app.bootstrap();
 
