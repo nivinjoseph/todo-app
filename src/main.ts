a2602c8687f5f994626a224108f45612f0020078
nivinjoseph <nivinjoseph@outlook.com>
2017-05-04 08:50:34 -0400

    finalized build steps

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { DefaultController } from "./controllers/default-controller";
 import { ConfigurationManager } from "n-config";
 
 
+console.log("mode", ConfigurationManager.getConfig("mode"));
+
 const app = new WebApp(ConfigurationManager.getConfig<number>("port"))
     .useViewResolutionRoot("src/controllers")
     .registerStaticFilePaths("src/client/dist")
